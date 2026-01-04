da17f55441b5d8a22349ea9445561add50861c7f
Julfa-11 <julfa2728@gmail.com>
2026-01-04 15:52:24 +0530

    Add files via upload

---
+++ image3.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.image(
+    "https://images.unsplash.com/photo-1530092285049-1c42085fd395?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Zmxvd2VyJTIwd2FsbHBhcGVyfGVufDB8fDB8fHww",
+    caption="Image from Internet"
+)
